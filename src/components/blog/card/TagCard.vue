9b9ef12a0922955a12b4bbd4f956da7332b20978
shota <inoueshota0108@gmail.com>
2024-03-07 00:00:12 +0900

    add style

---
+++ src/components/blog/card/TagCard.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import type { Tag } from "../../../lib/newt/types.ts";
+// Life Cycle Hooks
+import { computed } from "vue";
+
+interface Props {
+    tag: Tag;
+    isLinkDisabled: boolean
+}
+
+const { tag, isLinkDisabled } = withDefaults(defineProps<Props>(), {
+    isLinkDisabled: false,
+})
+
+const themeColor = computed<string>(() => {
+    return tag.parentCategory.themeColor
+})
+</script>
+
+<template>
+    <div class="tag">
+        <img :src="tag.icon.src" :alt="tag.icon?.altText ?? ''" v-if="tag.icon" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" v-else>
+            <path
+                d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+        </svg>
+        <label>{{ tag.name }}</label>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.tag {
+    background: v-bind(themeColor);
+    border-radius: 4px;
+    padding: 0.2rem;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+
+    img {
+        width: auto;
+        height: 0.8rem;
+        object-fit: contain;
+    }
+
+    svg {
+        width: auto;
+        height: 0.8rem;
+        fill: getColor(--text-primary-color);
+    }
+
+    label {
+        font-size: 0.8rem;
+        font-weight: 700;
+    }
+}
+</style>
