8ccc71d6a2cbdb009c55ec4398c12f4aded260b1
shota <inoueshota0108@gmail.com>
2024-03-13 23:18:22 +0900

    add style

--- src/components/blog/card/ArchiveCard.vue
+++ src/components/blog/card/ArchiveCard.vue
@@ -10,6 +10,13 @@ const { month, isDisabled } = defineProps<Props>()
 const cursor = isDisabled ? "none" : "pointer";
 const pointerEvents = isDisabled ? "none" : "visible";
 const opacity = isDisabled ? 0.5 : 1;
+
+const getBgColor = (month: number): string => {
+    const colorList = ["#0ea5e9", "#fb7185", "#22c55e", "#fbbf24"];
+    return colorList[Math.ceil(month / 3) - 1];
+}
+
+const bgColor = getBgColor(month);
 </script>
 
 <template>
@@ -22,28 +29,30 @@ const opacity = isDisabled ? 0.5 : 1;
 
 <style scoped lang="scss">
 .archive-month-card {
+    font-size: 2.4rem;
+    font-weight: 500;
     position: relative;
     display: block;
     width: 100%;
     height: 100%;
-    background: getColor(--bg-primary-color, v-bind(opacity));
+    color: getColor(--text-primary-color);
+    background: v-bind(bgColor);
     border: solid 1px getColor(--bg-secondary-color);
     pointer-events: v-bind(pointerEvents);
+    opacity: v-bind(opacity);
 
     &:active {
-        background: $rose;
+        font-size: 3rem;
+        font-weight: 600;
         transition: .2s ease;
     }
 
     .month {
-        color: getColor(--text-primary-color, v-bind(opacity));
         width: 100%;
         height: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
-        font-size: 2.4rem;
-        font-weight: 700;
         letter-spacing: 1px;
         cursor: v-bind(cursor);
     }
