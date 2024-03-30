80e6dfd44741257e8b10aff87f8ccd9efbb68a75
shota <inoueshota0108@gmail.com>
2024-03-30 20:55:15 +0900

    add anim

--- src/components/blog/article/PageTopButton.vue
+++ src/components/blog/article/PageTopButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, computed, ref } from "vue";
 import { svgPaths, svgViewBoxes } from "../../../lib/svg/paths";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -12,6 +12,21 @@ const transitionToTop = (): void => {
 		behavior: "smooth",
 	});
 };
+
+// Progress circle
+const radius = 42;
+
+const progressPercent = ref<number>(0);
+
+const circumference = 2 * Math.PI * radius;
+
+const progress = computed<number>(() => {
+	const calculated = 2 * Math.PI * (1 - progressPercent.value) * radius;
+	if (progressPercent.value > 1) {
+		return 0;
+	}
+	return calculated;
+});
 
 onMounted(() => {
 	const articleContent = document.querySelector(".article-content");
@@ -31,13 +46,23 @@ onMounted(() => {
 			toggleActions: "play reverse play reverse",
 		},
 	});
+
+	window.addEventListener("scroll", async () => {
+		const scrollAmount = window.scrollY;
+		const height = document.getElementById("article-content")!.scrollHeight;
+		progressPercent.value = scrollAmount / height;
+	});
 });
 </script>
 
 <template>
 	<button id="page-top-btn" type="button" @click="transitionToTop">
-		<svg xmlns="http://www.w3.org/2000/svg" :viewBox="svgViewBoxes.chevronUp">
+		<svg xmlns="http://www.w3.org/2000/svg" class="chevron" :viewBox="svgViewBoxes.chevronUp">
 			<path :d="svgPaths.chevronUp"></path>
+		</svg>
+		<svg xmlns="http://www.w3.org/2000/svg" class="progress-bar" viewBox="0 0 100 100">
+			<circle class="bar" cx="50" cy="50" :r="radius"></circle>
+			<circle class="bg" cx="50" cy="50" :r="radius"></circle>
 		</svg>
 	</button>
 </template>
@@ -46,21 +71,41 @@ onMounted(() => {
 #page-top-btn {
 	width: 42px;
 	height: 42px;
-	background: getColor(--bg-primary-color);
 	border-radius: 9999px;
-	border: solid 2px getColor(--border-color);
-	box-shadow: 0 0 5px getColor(--border-color);
 	z-index: 1000;
 
 	position: fixed;
 	bottom: 8px;
 	right: 8px;
 
-	svg {
+	.chevron {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 40%;
+		height: 40%;
+		fill: getColor(--theme-color);
+		z-index: 1001;
+	}
+
+	.progress-bar {
 		width: 100%;
 		height: 100%;
-		padding: 8px;
-		fill: getColor(--text-secondary-color);
+		transform: rotateZ(180deg);
+
+		.bg {
+			stroke: getColor(--bg-secondary-color);
+			fill: getColor(--bg-secondary-color);
+		}
+
+		.bar {
+			stroke-linecap: butt;
+			stroke-width: 16px;
+			stroke: getColor(--theme-color);
+			stroke-dasharray: v-bind(circumference);
+			stroke-dashoffset: v-bind(progress);
+		}
 	}
 }
 </style>
