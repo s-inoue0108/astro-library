42df7cad30a6ab9fd1049591196d628dcb00bd32
shota <inoueshota0108@gmail.com>
2024-04-04 11:46:20 +0900

    add style

--- src/components/common/form/ContactForm.tsx
+++ src/components/common/form/ContactForm.tsx
@@ -48,7 +48,9 @@ export default function ContactForm() {
 				});
 
 				if (response.ok) {
-					alert("内容を送信しました。確認メールをお送りいたしますので、メールボックスをご確認下さい。");
+					alert(
+						"内容を送信しました。確認メールをお送りいたしますので、メールボックスをご確認下さい。"
+					);
 					useEffect(() => {
 						reset();
 					}, [reset]);
@@ -76,20 +78,47 @@ export default function ContactForm() {
 					お名前
 				</label>
 				{errors?.name && (
-					<span id="error-name-required" aria-live="assertive" className={style.contactFormErrorMsg}>
+					<span
+						id="error-name-required"
+						aria-live="assertive"
+						className={style.contactFormErrorMsg}
+					>
 						{errors.name.message}
 					</span>
 				)}
-				<input id="name" {...register("name", { required: "未入力です", maxLength: { value: 50, message: "最大50文字です" } })} aria-describedby="error-name-required" className={style.contactFormInput} />
+				<input
+					id="name"
+					{...register("name", {
+						required: "未入力です",
+						maxLength: { value: 50, message: "最大50文字です" },
+					})}
+					aria-describedby="error-name-required"
+					className={style.contactFormInput}
+				/>
 				<label htmlFor="email" className={style.contactFormLabel}>
 					Eメールアドレス
 				</label>
 				{errors?.email && (
-					<span id="error-email-required" aria-live="assertive" className={style.contactFormErrorMsg}>
+					<span
+						id="error-email-required"
+						aria-live="assertive"
+						className={style.contactFormErrorMsg}
+					>
 						{errors.email.message}
 					</span>
 				)}
-				<input id="email" {...register("email", { required: "未入力です", maxLength: { value: 100, message: "最大100文字です" }, pattern: { value: /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/, message: "無効な形式です" } })} className={style.contactFormInput} />
+				<input
+					id="email"
+					{...register("email", {
+						required: "未入力です",
+						maxLength: { value: 100, message: "最大100文字です" },
+						pattern: {
+							value: /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+							message: "無効な形式です",
+						},
+					})}
+					className={style.contactFormInput}
+				/>
 				<label htmlFor="content" className={style.contactFormLabel}>
 					お問い合わせ内容
 					<button type="button" onClick={discard} className={style.contactFormDiscardBtn}>
@@ -99,11 +128,22 @@ export default function ContactForm() {
 					</button>
 				</label>
 				{errors?.content && (
-					<span id="error-content-required" aria-live="assertive" className={style.contactFormErrorMsg}>
+					<span
+						id="error-content-required"
+						aria-live="assertive"
+						className={style.contactFormErrorMsg}
+					>
 						{errors.content.message}
 					</span>
 				)}
-				<textarea id="content" {...register("content", { required: "未入力です", maxLength: { value: 1000, message: "最大1000文字です" } })} className={style.contactFormTextarea} />
+				<textarea
+					id="content"
+					{...register("content", {
+						required: "未入力です",
+						maxLength: { value: 1000, message: "最大1000文字です" },
+					})}
+					className={style.contactFormTextarea}
+				/>
 				<button type="submit" className={style.contactFormSubmitBtn}>
 					確認して送信
 				</button>
